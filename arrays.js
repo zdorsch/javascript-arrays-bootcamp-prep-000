f0706a9f9b6996056e285956065ed79cc00e5067
Zach Dorsch <zachary.dorsch@gmail.com>
2018-03-21 17:08:56 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,40 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles',
+
+];
+
+
+ var sisters = ["gabby","Drew","Julie","Dannielle","lauren"];
+ var myself = "Zach";
+
+function addElementToBeginningOfArray(sisters,myself){
+  const family=  [`${myself}`,...sisters];
+  return family;
+}
+
+var dogs = ["Rudy", "Jack"];
+var cats = "Tootsie";
+
+function destructivelyAddElementToBeginningOfArray(dogs,cats){
+  dogs.unshift(`${cats}`);
+  return dogs;
+}
+
+function addElementToEndOfArray(sisters,myself){
+  const family=  [...sisters,`${myself}`];
+  return family;
+}
+  
+  var games = ["poker","tetris"];
+  var sport = "golf";
+  
+  function destructivelyAddElementToEndOfArray(games,sport){
+    games.push(`${sport}`)
+    return games;
+    }
+    
+    
+    
